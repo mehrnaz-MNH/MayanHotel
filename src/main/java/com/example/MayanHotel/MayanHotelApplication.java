cef162b714735ab2f255752c74957f9315d7ca43
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-08-14 13:10:41 +1000

    Setup the project and file structure

---
+++ src/main/java/com/example/MayanHotel/MayanHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.MayanHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayanHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayanHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MayanHotel/MayanHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MayanHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayanHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
